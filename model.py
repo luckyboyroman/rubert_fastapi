6053cf6f4c0cdf4a4cce1ec084bd9e76c01d746b
rigubenok <gdz-larin.ru@yandex.ru>
2026-08-01 15:11:47 +0300

    Проверка работоспособности модели rubert для классификации текстов

---
+++ model.py
@@ -0,0 +1,35 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+HF_TOKEN = os.getenv("HF_TOKEN")
+HF_HOME = os.getenv("HF_HOME")
+
+os.environ["HF_TOKEN"] = HF_TOKEN
+os.environ['HF_HOME'] = HF_HOME
+
+
+# Загружаем модель и токенизатор
+MODEL_ID = "Kostya165/rubert_tiny2_russian_emotion_sentiment"
+tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
+model     = AutoModelForSequenceClassification.from_pretrained(MODEL_ID)
+model.eval()
+
+texts = [
+    "Сегодня отличный день!",
+    "Меня это всё бесит и раздражает."
+]
+
+# Токенизация
+enc = tokenizer(texts, padding=True, truncation=True, max_length=128, return_tensors="pt")
+with torch.no_grad():
+    logits = model(**enc).logits
+    preds = logits.argmax(dim=-1).tolist()
+
+# Преобразуем ID обратно в метки
+id2label = model.config.id2label
+labels = [id2label[p] for p in preds]
+print(labels)  # например: ['positive', 'aggression']
